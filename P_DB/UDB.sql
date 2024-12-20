00ff8c06d0194715aac81de42e34a40b0b47f28e
ssoik77 <dksrud6554@naver.com>
2024-12-20 15:18:37 +0900

    DB 업데이트

--- P_DB/UDB.sql
+++ P_DB/UDB.sql
@@ -1,30 +1,40 @@
 CREATE DATABASE post_underdog;
 USE post_underdog;
 
-
-drop table member_info;
-drop table employee_info;
-
 CREATE TABLE member_info(
-
-a_authority TINYINT(1) NOT NULL DEFAULT 0,
-p_authority TINYINT(1) NOT NULL DEFAULT 0,
-e_authority TINYINT(1) NOT NULL DEFAULT 0,
-m_id CHAR(20) default null,
-m_pw CHAR(20) default null
-); 
-
-
-CREATE TABLE employee_info(
-e_num int auto_increment primary KEY,
-e_name CHAR(10) default null,
-e_birth DATE default null,
-e_carrier CHAR(10) default null,
-e_tel_num VARCHAR(20) default null
+a_authority TINYINT NOT NULL DEFAULT 0,
+p_authority TINYINT NOT NULL DEFAULT 0,
+m_authority TINYINT NOT NULL DEFAULT 0,
+m_id CHAR(20),
+m_pw VARCHAR(20),
+m_key int auto_increment primary key
 );
 
-select * from member_info;
-select * from employee_info;
+drop table member_info;
 
+DESCRIBE member_info;	
 
+CREATE TABLE employee_info(
+e_num int auto_increment primary key,
+e_name CHAR(10),
+e_birth DATE,
+e_carrier CHAR(10),
+e_tel_num CHAR(11),
+m_key int,
+FOREIGN KEY (m_key) REFERENCES member_info(m_key)
+);
 
+drop table employee_info;
+
+DESCRIBE employee_info;	
+-- 아래로 코드 테스트용 칼럼데이터 추가코드 (없어도 됨)
+INSERT INTO member_info (a_authority, p_authority, m_authority, m_id, m_pw)
+VALUES (0, 0, 0, 'user1234', 'password1235');
+
+INSERT INTO employee_info (e_name, e_birth, e_carrier, e_tel_num, m_key)
+VALUES ('엄홍길', '1990-01-01', 'SKT', '01012345678', LAST_INSERT_ID());
+
+SELECT m.m_id
+FROM member_info m
+JOIN employee_info e ON m.m_key = e.m_key
+WHERE e.e_tel_num = '01012345678' AND e.e_carrier = 'SKT';
