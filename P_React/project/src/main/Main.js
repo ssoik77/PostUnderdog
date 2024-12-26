228cf8b55adfbe0aeb84eb1d2a17ceb3e3b028d3
stoneboy <pjhpjy0704@naver.com>
2024-12-26 17:16:28 +0900

    리액트 상품 관리 페이지 이동 추가

--- P_React/project/src/main/Main.js
+++ P_React/project/src/main/Main.js
@@ -4,12 +4,17 @@ import '../mypage/Mypage.js';
 
 const Main = () => {
   const memberInfo = (JSON.parse(sessionStorage.getItem("authority")))[0];
+
   // 내 정보 팝업 열기 함수
   const openPopup = () => {
     const popupFeatures = "width=800,height=600,top=100,left=100,resizable=no,scrollbars=yes";
     window.open("../Mypage", "내 정보", popupFeatures);
   };
 
+  // 상품 관리 페이지로 이동
+  const goToProductManage = () => {
+    window.location.href = "/productmanage"; // 페이지 이동
+  };
 
   return (
     <div>
@@ -20,6 +25,12 @@ const Main = () => {
       <button id={styles.infoButton} onClick={openPopup} className={styles.button}>
         내 정보
       </button>
+
+      {/* 상품 관리 페이지 이동 버튼 */}
+      <button id={styles.productManageButton} onClick={goToProductManage} className={styles.button}>
+        상품 관리 페이지
+      </button>
+
     </div>
   );
 };
