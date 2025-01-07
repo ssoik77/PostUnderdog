8f30e1841dd4ffce69259e110121c0fefcb58f5b
stoneboy <pjhpjy0704@naver.com>
2025-01-07 10:44:10 +0900

    문의 탭 페이지 추가

---
+++ P_React/project/src/help/HelpManage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './HelpManage.module.css';
+
+const HelpManage = () => {
+  return (
+    <div className={styles.container}>
+        <h1>문의사항</h1>
+    </div>
+  );
+};
+
+export default HelpManage;
