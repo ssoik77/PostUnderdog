592d1c8a47d9f382a82add7d64c8d19da2645d8d
stoneboy <pjhpjy0704@naver.com>
2025-01-08 17:44:43 +0900

    라우터 수정

--- P_React/project/src/router/AppRouter.js
+++ P_React/project/src/router/AppRouter.js
@@ -4,9 +4,9 @@ import Register from "../register/Register"
 import Find from "../find/Find"
 import Main from "../main/Main"
 import Mypage from "../mypage/Mypage"
-import Emp from "../employee/Employeemain"
+import Employeemain from "../employee/Employeemain";
+import Emp from "../employee/Employeemain";
 import App from "../App";
-import Organization from '../employee/Organization';
 
 function AppRouter() {
     return (
@@ -17,8 +17,8 @@ function AppRouter() {
           <Route path="/find" element={<Find/>} />
           <Route path="/main" element={<Main/>} />
           <Route path="/mypage" element={<Mypage/>} />
+          <Route path="/employeemain" element={<Employeemain/>} />
           <Route path="/emp" element={<Emp/>} />
-          <Route path="/organization" element={<Organization/>} />
         </Routes>
       </Router>
     );
