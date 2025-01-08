bb74571d4c18b1c3c84b323f0bdaf1be1efc3e6a
ssoik77 <dksrud6554@naver.com>
2025-01-08 17:47:04 +0900

    상품 관리 페이지의 상품 추가 페이지 리액트 기능 부분 완료 상품 리스트 페이지 뼈대 완료

--- P_React/project/src/product/ProductManage.js
+++ P_React/project/src/product/ProductManage.js
@@ -1,19 +1,51 @@
-import React from 'react';
 import styles from './ProductManage.module.css';
 
 const ProductManage = () => {
-  
-  
+
+  const goAddPage = () => {
+    window.location.href = "/productAdd";
+  }
+
   return (
     <>
-    <div className={styles.container}>
-      <h3>POST UNDERDOG</h3>
-      <h1>상품 관리 페이지</h1>
-    </div>
-    <div id={styles.productMenu}>
-    <button id={styles.productList}>상품 리스트</button>
-    <button id={styles.productAdd}>상품 추가</button>
-    </div>
+      <div className={styles.container}>
+        <h3>POST UNDERDOG</h3>
+        <h1>상품 관리 페이지</h1>
+      </div>
+      <div id={styles.menu}>
+        <button id={styles.listButton} className={styles.menuButton} style={{ backgroundColor: 'blue' }}>상품 리스트</button>
+        <button id={styles.addButton} className={styles.menuButton} onClick={goAddPage}>상품 추가</button>
+      </div>
+      <div id={styles.main}>
+        <div id={styles.list}>
+          <table>
+              <td className={styles.product}>
+                상품 테스트
+              </td>
+              <td className={styles.product}>
+                상품 테스트
+              </td>
+              <td className={styles.product}>
+                상품 테스트
+              </td>
+              <td className={styles.product}>
+                상품 테스트
+              </td>
+              <td className={styles.product}>
+                상품 테스트
+              </td>
+              <td className={styles.product}>
+                상품 테스트
+              </td>
+              <td className={styles.product}>
+                상품 테스트
+              </td>
+              <td className={styles.product}>
+                상품 테스트
+              </td>
+          </table>
+        </div>
+      </div>
     </>
   );
 };
--- P_React/project/src/router/AppRouter.js
+++ P_React/project/src/router/AppRouter.js
@@ -1,12 +1,13 @@
 import React from "react";
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Register from "../register/Register"
-import Find from "../find/Find"
-import Main from "../main/Main"
-import Mypage from "../mypage/Mypage"
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Register from "../register/Register";
+import Find from "../find/Find";
+import Main from "../main/Main";
+import Mypage from "../mypage/Mypage";
 import ProductManage from "../product/ProductManage";
+import ProductAdd from "../product/ProductAdd";
 import EmployeeManage from "../employee/EmployeeManage";
-import HelpManage from "../help/HelpManage"
+import HelpManage from "../help/HelpManage";
 import App from "../App";
 
 function AppRouter() {
@@ -18,9 +19,11 @@ function AppRouter() {
           <Route path="/find" element={<Find/>} />
           <Route path="/main" element={<Main/>} />
           <Route path="/mypage" element={<Mypage/>} />
-          <Route path="/productmanage" element={<ProductManage />} />
+          <Route path="/productmanage" element={<ProductManage />}/>
+          <Route path="/ProductAdd" element={<ProductAdd/>}/>
           <Route path="/employeemanage" element={<EmployeeManage />} />
           <Route path="/helpmanage" element={<HelpManage/>}/>
+
         </Routes>
       </Router>
     );
